e4f7e196fddb1c13565829e13019bd58a5dbef8d
Paweł <pawelokrasa198@gmail.com>
2018-11-24 13:21:47 +0100

    Services add dbcontext

--- CatchUp/Models/Lessons/Lesson.cs
+++ CatchUp/Models/Lessons/Lesson.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CatchUp.Models.Lessons
 {
     public class Lesson
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; }
         public int OrderId { get; set; }
         public virtual Order Order { get; set; }
     }
@@ -15,7 +16,12 @@ namespace CatchUp.Models.Lessons
     {
         public void Configure(EntityTypeBuilder<Lesson> b)
         {
-            //to do fluent api
+            b.HasKey(l => l.Id);
+            b.Property(l => l.Id).UseSqlServerIdentityColumn();
+            //b.Property(l => l.Id).ForSqlServerUseSequenceHiLo<int>("DbSequence").StartsAt(1).IncremetsBy(1);
+            b.Property(l => l.OrderId);
+            b.Property(l => l.Order).IsRequired();
+            b.HasOne(l => l.Order);
         }
     }
 }
