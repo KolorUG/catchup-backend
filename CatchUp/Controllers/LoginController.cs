76ff81a4b6c557a4b1ad6ef5fd2a78311ac7eb3f
Sebastian_Dremo <sebastiandremo@gmail.com>
2018-11-24 16:00:07 +0100

    Added LoginController

--- CatchUp/Controllers/LoginController.cs
+++ CatchUp/Controllers/LoginController.cs
@@ -1,7 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
 namespace CatchUp.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class LoginController : ControllerBase
     {
-        
+        private readonly ILoginRepository _loginRepository;
+        private static string _password;
+        public LoginController(ILoginRepository loginRepository)
+        {
+            this._loginRepository = loginRepository;
+        }
+        [HttpGet]
+        public ActionResult Login([FromBody]string login, string password)
+        {
+            
+        }
     }
 }
