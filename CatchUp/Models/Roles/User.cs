0c643d3d6df49f6341a80282469ff33e85997f79
Sebastian_Dremo <sebastiandremo@gmail.com>
2018-11-24 17:11:15 +0100

    Added User

---
+++ CatchUp/Models/Roles/User.cs
@@ -0,0 +1,10 @@
+namespace CatchUp.Models.Roles
+{
+    public class User : BaseEntity
+    {
+        public int Id { get; set; }
+        public string Login { get; set;}
+        public string Password { get; set; }
+        public int  { get; set; }
+    }
+}
